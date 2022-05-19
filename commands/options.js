60562b85803351cc9402a48da1d9c98124bbf027
dumpy <andy@dumpyy.xyz>
2022-05-19 20:26:01 +1000

    options command

---
+++ commands/options.js
@@ -0,0 +1,89 @@
+const { Permissions, MessageEmbed } = require("discord.js");
+const db = require("quick.db");
+module.exports = {
+  name: "options",
+  description: "Options command",
+  aliases: [],
+  usage: "",
+  guildOnly: false,
+  args: false,
+  permissions: {
+    bot: [],
+    user: [],
+  },
+  execute: (message, args, client) => {
+    if (args[0] === "help") {
+      return message.channel.send({
+        embed: [
+          {
+            color: "#F04A47",
+            title: "Options",
+            description: "This is a list of options for the bot!",
+          },
+        ],
+      });
+    }
+    switch (args[0]) {
+      case "3warn":
+        if (!message.member.hasPermission("KICK_MEMBERS")) {
+          return message.channel.send({
+            embeds: [
+              {
+                color: "#F04A47",
+                title: "Missing KICK MEMBERS permission",
+                description:
+                  "You need to have the KICK MEMBERS permission to use this command!",
+              },
+            ],
+          });
+        } else {
+          switch (args[1]) {
+            case "kick":
+              db.set(`${message.guild.id}.3warn`, 0);
+              return message.channel.send({
+                embeds: [
+                  {
+                    color: "#F04A47",
+                    title: "3 Warn",
+                    description: "You have set the 3 warn to kick",
+                  },
+                ],
+              });
+            case "ban":
+              db.set(`${message.guild.id}.3warn`, 1);
+              return message.channel.send({
+                embeds: [
+                  {
+                    color: "#F04A47",
+                    title: "3 Warn",
+                    description: "You have set the 3 warn to ban",
+                  },
+                ],
+              });
+            case "nothing":
+              db.set(`${message.guild.id}.3warn`, 2);
+              return message.channel.send({
+                embeds: [
+                  {
+                    color: "#F04A47",
+                    title: "3 Warn",
+                    description: "You have set the 3 warn to nothing",
+                  },
+                ],
+              });
+            default:
+              return message.channel.send({
+                embeds: [
+                  {
+                    color: "#F04A47",
+                    title: "3 Warn",
+                    description:
+                      "Please provide a valid option (kick, ban, nothing)",
+                  },
+                ],
+              });
+          }
+        }
+    }
+  },
+};
--- commands/warn.js
+++ commands/warn.js
@@ -1,109 +1,133 @@
-const { Permissions, MessageEmbed } = require("discord.js")
-const db = require("quick.db")
+const { Permissions, MessageEmbed } = require("discord.js");
+const db = require("quick.db");
 
-
-let wrong = "#F04A47"
+let wrong = "#F04A47";
 
 module.exports = {
-    name: 'warn',
-    description: 'warn command',
-    usage: '<user> <reason>',
-    aliases: ['none'],
-	  guildOnly: false, //true if only used in server
-	  args: true, //true if the command cant run without arguments
-    category: "moderation",
-    execute: async (message, args, client) => {
-      try {
+  name: "warn",
+  description: "warn command",
+  usage: "<user> <reason>",
+  aliases: ["none"],
+  guildOnly: false, //true if only used in server
+  args: true, //true if the command cant run without arguments
+  category: "moderation",
+  execute: async (message, args, client) => {
+    try {
+      if (!message.member.permissions.has(Permissions.FLAGS.ADMINISTRATOR)) {
+        const embed = new MessageEmbed().setDescription(
+          "Missing KICK MEMBERS permission"
+        );
+        return message.channel.send(embed);
+      }
 
-        if (!message.member.permissions.has(Permissions.FLAGS.ADMINISTRATOR)) {
-          const embed = new MessageEmbed()
-          .setDescription("Missing KICK MEMBERS permission")
-          return message.channel.send(embed)
-        }
-
-        const target = message.mentions.users.first()
-        if(!target) {
-          const embed = new MessageEmbed()
-          .setDescription("Mention a member please.")
-          .setColor(wrong)
-
-          return message.channel.send({
-            embeds: [embed],
-          })
-
-        }
-
-        if (target.bot){
-          const embed = new MessageEmbed()
-          .setDescription("You cannot warn bots")
-          .setColor(wrong)
-          return message.channel.send({
-            embeds: [embed],
-          })
-        }
-        if (target.id === message.author.id) {
-          const embed = new MessageEmbed()
-          .setDescription("You cannot warn yourself.")
-          .setColor(wrong)
-          return message.channel.send({
-            embeds: [embed],
-          })
-        }
-
-        if(message.member.roles.highest.comparePositionTo(message.mentions.members.first().roles.highest) < 1) {
-          const embed = new MessageEmbed()
-          .setDescription("You cannot warn people higher than you!")
-          .setColor(wrong)
-          return message.channel.send({
-            embeds: [embed],
-          })
-        }
-
-        let reason = args.slice(1).join(" ")
-
-        if(!reason) {
-          const embed = new MessageEmbed()
-          .setDescription("Please provide a reason")
-          .setColor(wrong)
-          return message.channel.send({
-            embeds: [embed],
-          })
-        }
-
-
-        db.add(`warns_${target.id}`, 1)
-        var x = db.get(`warns_${target.id}`)
-
-        
+      const target = message.mentions.users.first();
+      if (!target) {
         const embed = new MessageEmbed()
-        .setDescription(`Successfully warned ${target.username} for reason ${reason}, this user now has ${x} warning(s)`)
-        .setColor(wrong)
+          .setDescription("Mention a member please.")
+          .setColor(wrong);
+
+        return message.channel.send({
+          embeds: [embed],
+        });
+      }
+
+      if (target.bot) {
+        const embed = new MessageEmbed()
+          .setDescription("You cannot warn bots")
+          .setColor(wrong);
+        return message.channel.send({
+          embeds: [embed],
+        });
+      }
+      if (target.id === message.author.id) {
+        const embed = new MessageEmbed()
+          .setDescription("You cannot warn yourself.")
+          .setColor(wrong);
+        return message.channel.send({
+          embeds: [embed],
+        });
+      }
+
+      if (
+        message.member.roles.highest.comparePositionTo(
+          message.mentions.members.first().roles.highest
+        ) < 1
+      ) {
+        const embed = new MessageEmbed()
+          .setDescription("You cannot warn people higher than you!")
+          .setColor(wrong);
+        return message.channel.send({
+          embeds: [embed],
+        });
+      }
+
+      let reason = args.slice(1).join(" ");
+
+      if (!reason) {
+        const embed = new MessageEmbed()
+          .setDescription("Please provide a reason")
+          .setColor(wrong);
+        return message.channel.send({
+          embeds: [embed],
+        });
+      }
+      db.add(`warns_${message.guild.id}_${target.id}`, 1);
+      var x = db.get(`warns_$${target.id}`);
+      var threewarn = db.get(`${message.guild.id}.3warn`);
+      if (threewarn === 0 && x >= 3) {
+        target.ban()
+        return message.channel.send({
+          embeds: [
+            {
+              color: "#F04A47",
+              title: "3 Warn",
+              description: `${target} has been **banned**`,
+            },
+          ],
+        });
+
+      } else if (threewarn === 1 && x >= 3) {
+        target.kick()
+        return message.channel.send({
+          embeds: [
+            {
+              color: "#F04A47",
+              title: "3 Warn",
+              description: `${target} has been **kicked**`,
+            },
+          ],
+        });
+      }
+
+      const embed = new MessageEmbed()
+        .setDescription(
+          `Successfully warned ${target.username} for reason ${reason}, this user now has ${x} warning(s)`
+        )
+        .setColor(wrong);
+      message.channel.send({
+        embeds: [embed],
+      });
+
+      try {
+        const embed = new MessageEmbed()
+          .setDescription(
+            `You have been warned in ${message.guild.name}, you now have ${x} warning(s)`
+          )
+          .setFooter(`Reason: ${reason}`)
+          .setColor(wrong);
+        target.send({
+          embeds: [embed],
+        });
+      } catch (err) {
+        const embed = new MessageEmbed()
+          .setDescription("Could not DM user.")
+          .setColor(wrong);
         message.channel.send({
           embeds: [embed],
-        })
-
-        try {
-          const embed = new MessageEmbed()
-          .setDescription(`You have been warned in ${message.guild.name}, you now have ${x} infractions`)
-          .setFooter(`Reason: ${reason}`)
-          .setColor(wrong)
-          target.send({
-            embeds: [embed],
-          })
-        } catch (err) {
-          const embed = new MessageEmbed()
-          .setDescription("Could not DM user.")
-          .setColor(wrong)
-          message.channel.send({
-            embeds: [embed],
-          })
-        }
-        
-
-
-      } catch (err) {
-        console.log(err)
-
+        });
       }
+    } catch (err) {
+      console.log(err);
     }
-}
+  },
+};
