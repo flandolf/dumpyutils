0e2b71431bce949e7925f4e13fc6d62408e650b3
dumpy <dumpyee09@gmail.com>
2022-11-13 20:31:57 +1100

    fixes

--- c.py
+++ c.py
@@ -1,16 +1,19 @@
 import json
-
-
 msg = input("Enter commit message: ")
 # update package.json
 with open("package.json", "r") as f:
     data = json.load(f)
     nver = int(str(data['version']).split('.')[2]) + 1
-    data['version'] = data['version'][:-1] + str(nver)
+    if nver > 9:
+        nver = 0
+        mver = int(str(data['version']).split('.')[1]) + 1
+        data['version'] = str(data['version']).split('.')[0] + "." + str(mver) + "." + str(nver)
+    else:
+        data['version'] = str(data['version']).split('.')[0] + "." + str(data['version']).split('.')[1] + "." + str(nver)
     with open("package.json", "w") as w:
         json.dump(data, w, indent=4)
 
 import os
 os.system("git add .")
-os.system("git commit -m " + r"\"" + msg + r"\"")
+os.system("git commit -m " + "\"" + msg + "\"")
 os.system("git push")
